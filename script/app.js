c55a2d3008075d1cb1342b08a1c5cf686466d33b
Guillaume LE DEVIN <guillaume.le.devin@gmail.com>
2022-02-10 23:40:49 +0000

    Menu + presentation + slider

---
+++ script/app.js
@@ -0,0 +1,31 @@
+const itemSlide = document.querySelectorAll('.container-slides img');
+const nbSlide = itemSlide.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let count = 0;
+
+suivant.addEventListener('click', slideSuivante);
+function slideSuivante() {
+    itemSlide[count].classList.remove('active');
+
+    if(count < nbSlide - 1) {
+        count++;
+    } else {
+        count = 0;
+    }
+
+    itemSlide[count].classList.add('active');
+}
+
+precedent.addEventListener('click', slidePrecedente);
+function slidePrecedente() {
+    itemSlide[count].classList.remove('active');
+
+    if(count > 0) {
+        count--;
+    } else {
+        count = nbSlide - 1;
+    }
+
+    itemSlide[count].classList.add('active');
+}
